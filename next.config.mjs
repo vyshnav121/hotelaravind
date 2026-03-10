fae0e9b6ea47592964fa0ed513e19794173a419e
vyshnav121 <vyshnavkuttu121@gmail.com>
2026-03-10 12:12:47 +0530

    chore: add myfoodstory.com to next.config.mjs remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,10 @@ const nextConfig = {
                 protocol: "https",
                 hostname: "www.theraviz.com",
             },
+            {
+                protocol: "https",
+                hostname: "myfoodstory.com",
+            },
         ],
     },
 };
